f8e5f3d4af882c3d90973f1c2a2ceed34fb312f3
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2026-06-28 03:16:03 -0600

    feat: docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
 import routes from "./routes";
 import { errorHandler } from "./middleware/errorHandler";
 
@@ -7,6 +9,23 @@ export const createApp = () => {
 
   app.use(express.json());
   app.use(routes);
+
+  const frontendDistPath = path.resolve(__dirname, "../frontend/dist");
+  if (fs.existsSync(frontendDistPath)) {
+    app.use(express.static(frontendDistPath));
+    app.get("*", (req, res, next) => {
+      if (req.method !== "GET") {
+        return next();
+      }
+
+      res.sendFile(path.join(frontendDistPath, "index.html"), (err) => {
+        if (err) {
+          next(err);
+        }
+      });
+    });
+  }
+
   app.use(errorHandler);
 
   return app;
