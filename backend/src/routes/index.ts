e2a58bd7a614575cd0c7d197b78f41beed21ce35
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2026-06-28 10:53:11 -0600

    feat: album api (#10)

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth";
 import artistsRouter from "./artists";
 import healthRouter from "./health";
 import songsRouter from "./songs";
+import albumsRouter from "./albums";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use(healthRouter);
 router.use("/auth", authRouter);
 router.use(artistsRouter);
 router.use(songsRouter);
+router.use(albumsRouter);
 
 export default router;
